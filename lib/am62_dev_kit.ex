311cb3369e19cf7bb18b2669389f1fba43d61558
Alex McLain <alex@alexmclain.com>
2024-06-11 00:26:19 -0700

    Get CPU and memory temperature

--- lib/am62_dev_kit.ex
+++ lib/am62_dev_kit.ex
@@ -2,4 +2,45 @@ defmodule AM62DevKit do
   @moduledoc """
   Nerves tooling for the AM62 dev kits.
   """
+
+  @doc """
+  Get the temperature of the CPU.
+  """
+  @spec cpu_temp(opts :: list) :: float
+  def cpu_temp(opts \\ []) do
+    i2c_ref = opts[:i2c_ref] || i2c_open!("i2c-1")
+    temperature = tmp_get_temp(i2c_ref, 0x48)
+
+    unless opts[:i2c_ref],
+      do: Circuits.I2C.close(i2c_ref)
+
+    temperature
+  end
+
+  @doc """
+  Get the temperature of the memory.
+  """
+  @spec memory_temp(opts :: list) :: float
+  def memory_temp(opts \\ []) do
+    i2c_ref = opts[:i2c_ref] || i2c_open!("i2c-1")
+    temperature = tmp_get_temp(i2c_ref, 0x49)
+
+    unless opts[:i2c_ref],
+      do: Circuits.I2C.close(i2c_ref)
+
+    temperature
+  end
+
+  defp i2c_open!(name) do
+    {:ok, ref} = Circuits.I2C.open(name)
+    ref
+  end
+
+  # Texas Instruments TMPxxx temperature sensor
+  defp tmp_get_temp(i2c_ref, address) do
+    {:ok, data} = Circuits.I2C.read(i2c_ref, address, 2)
+
+    <<temperature::signed-big-integer-size(12), _rest::size(4)>> = data
+    temperature * 0.0625
+  end
 end
